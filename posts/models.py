a72a74275b1332a095fc9a149681952337b4f89a
Maksim Pukshlis <maksimpusklis7@gmail.com>
2020-12-10 20:21:54 +0300

    init

---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Post(models.Model):
+    """Модель постов"""
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-data_created']
+
+
+class Vote(models.Model):
+    """Модель голосов"""
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
